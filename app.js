d57193c5f2ee0248ebcaa97d68b21622f8386384
Theodoros Maslarinos <thmaslarinos@gmail.com>
2024-05-29 11:22:04 +0300

    s

---
+++ app.js
@@ -0,0 +1,23 @@
+(function(){
+    'use strict';
+    
+    angular.module('app', [])
+    .controller('nameCalulatorController', function($scope){
+        $scope.name = "";
+        $scope.totalValue = 0;
+
+        $scope.displayNameNumeric = function(){
+            $scope.totalValue = calculateNumeric($scope.name)
+        }
+    });
+    
+
+    function calculateNumeric(string) {
+        var results = 0;
+        for(var i = 0; i < string.length; i++) {
+            results += string.charCodeAt(i);
+        }
+        return results;
+    };
+
+})();
