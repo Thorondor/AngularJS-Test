890e600842f81bcc7af81658141d3839ed621361
Thorondor <thmaslarinos@gmail.com>
2024-05-29 10:50:16 +0300

    t

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 (function(){
     'use strict';
-    angular.module('myFirstApp', [])
-    .controller('MyFirstController', function(){
+    
+    angular.module('app', [])
+    .controller('nameCalulatorController', function($scope){
+        $scope.name = "";
+        $scope.totalValue = 0;
 
+        $scope.displayNameNumeric = function(){
+            $scope.totalValue = calculateNumeric($scope.name)
+        }
     });
+
+    function calculateNumeric(string) {
+        var results = 0;
+        for(var i = 0; i < string.length; i++) {
+            results += string.charCodeAt(i);
+        }
+        return results;
+    };
 
 })();
